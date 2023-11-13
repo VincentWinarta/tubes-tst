e1c408de7fcc70f23b00067a2d19f727525f6ea2
VincentWinarta <66663413+VincentWinarta@users.noreply.github.com>
2023-11-14 05:37:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,129 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status, APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.encoders import jsonable_encoder
+import jwt
 import json
 from pydantic import BaseModel
-from datetime import datetime
+from datetime import datetime, timedelta
 import geocoder
 import time
+from passlib.hash import bcrypt
 
 class users(BaseModel):
-	userid: int
-	firstname: str
-	lastname: str
-	email: str
-	
+    userid: int
+    firstname: str
+    lastname: str
+    email: str
+
 class orders(BaseModel):
-	orderid: int
-	userid: int
-	orderstart: datetime
-	orderstop: datetime
-	duration : int
-	
-json_filename="main.json"
-with open(json_filename,"r") as read_file:
-	data = json.load(read_file)
-	
+    orderid: int
+    userid: int
+    orderstart: datetime
+    orderstop: datetime
+    duration: int
+
+json_filename = "main.json"
+with open(json_filename, "r") as read_file:
+    data = json.load(read_file)
+
+auth = APIRouter(tags=["Authentication"],)
+user = APIRouter(tags=["Users"],)
+order = APIRouter(tags=["Orders"],)
+loca = APIRouter(tags=["Location"],)
+
+
+class signin_user:
+    def __init__(self, id, username, pass_hash):
+        self.id = id
+        self.username = username
+        self.pass_hash = pass_hash
+
+    def verify_password(self, password):
+        return bcrypt.verify(password, self.pass_hash)
+
+def write_data(data):
+    with open("main.json", "w") as write_file:
+        json.dump(data, write_file, indent=4)
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
 app = FastAPI()
-@app.get('/users')
-async def read_all_users():
+JWT_SECRET = 'coworkingsecret'
+ALGORITHM = 'HS256'
+
+
+
+def get_user_by_username(username):
+    for desain_user in data['signin_user']:
+        if desain_user['username'] == username:
+            return desain_user
+    return None
+
+def authenticate_user(username: str, password: str):
+    user_data = get_user_by_username(username)
+    if not user_data:
+        return None
+
+    user = signin_user(id=user_data['id'], username=user_data['username'], pass_hash=user_data['pass_hash'])
+
+    if not user.verify_password(password):
+        return None
+
+    return user
+
+
+@auth.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
+    token = jwt.encode({'id': user.id, 'username': user.username}, JWT_SECRET, algorithm=ALGORITHM)
+
+    return {'access_token': token, 'token_type': 'bearer'}
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = get_user_by_username(payload.get('username'))
+        return signin_user(id=user['id'], username=user['username'], pass_hash=user['pass_hash'])
+    except:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, 
+            detail='Invalid username or password'
+        )
+
+@auth.post('/signin_user')
+async def create_user(username: str, password: str):
+    last_user_id = data['signin_user'][-1]['id'] if data['signin_user'] else 0
+    user_id = last_user_id + 1
+    user = jsonable_encoder(signin_user(id=user_id, username=username, pass_hash=bcrypt.hash(password)))
+    data['signin_user'].append(user)
+    write_data(data)
+    return {'message': 'User created successfully'}
+
+@auth.get('/signin_user/me')
+async def get_user(user: signin_user = Depends(get_current_user)):
+    return {'id': user.id, 'username': user.username}
+
+@user.get('/users')
+async def read_all_users(user: signin_user = Depends(get_current_user)):
 	return data['users']
 
-@app.get('/test')
+@loca.get('/test')
 async def loc():
 	g=geocoder.ip('me')
 	return g.latlng
 
-@app.get('/orders')
-async def read_all_orders():
+@order.get('/orders')
+async def read_all_orders(user: signin_user = Depends(get_current_user)):
 	return data['orders']
 
-@app.post('/users')
+@user.post('/users')
 async def add_user(userid: int, firstname: str, lastname: str, email: str):
 	item_found = False
 	for user_item in data['users']:
@@ -64,8 +150,8 @@ async def add_user(userid: int, firstname: str, lastname: str, email: str):
 		status_code=404, detail=f'item not found'
 	)
 
-@app.post('/orders')
-async def add_order(orderid: int, userid: int):
+@order.post('/orders')
+async def add_order(orderid: int, userid: int, user: signin_user = Depends(get_current_user)):
 	item_found = False
 	for order_item in data['orders']:
 		if order_item['orderid'] == orderid:
@@ -101,8 +187,8 @@ async def add_order(orderid: int, userid: int):
 		status_code=404, detail=f'item not found'
 	)
     
-@app.delete('/orders/{orderid}')
-async def delete_order(orderid: int):
+@order.delete('/orders/{orderid}')
+async def delete_order(orderid: int, user: signin_user = Depends(get_current_user)):
 	for order_item in data['orders']:
 		if order_item['orderid'] == orderid:
 			data['orders'].remove(order_item)
@@ -116,3 +202,9 @@ async def delete_order(orderid: int):
 	raise HTTPException(
 	status_code=404, detail=f'item not found'
 	)
+
+
+app.include_router(auth)
+app.include_router(user)
+app.include_router(order)
+app.include_router(loca)
