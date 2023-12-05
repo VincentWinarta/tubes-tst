61b7b6b9fb3631bd97dd5d369a4038f037778343
VincentWinarta <66663413+VincentWinarta@users.noreply.github.com>
2023-12-05 21:22:02 +0700

    Add files via upload

---
+++ req.py
@@ -0,0 +1,17 @@
+import requests, json
+
+url = 'http://20.247.130.142/'
+
+
+def get_token():
+    token_url = url+'token'
+    token_response = requests.post(token_url, data={'username': 'hilmi', 'password': 'secret'})
+    token = token_response.json().get('access_token')
+    return token
+
+def get_reservations():
+    headers = {'Authorization': f'Bearer {get_token()}'}
+    reservations = requests.get(url+'reservations', headers=headers)
+    return reservations.json()
+
+
