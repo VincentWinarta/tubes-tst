d083f4aedebab489c77463755457f76f13442c60
VincentWinarta <66663413+VincentWinarta@users.noreply.github.com>
2023-12-05 21:19:42 +0700

    integratedapi main.py

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ from datetime import datetime, timedelta
 import geocoder
 import time
 from passlib.hash import bcrypt
+import requests
+from location import get_location
+from req import get_reservations
+from pymongo import MongoClient
+
+client = MongoClient("mongodb+srv://vincentw:123@cluster0.juxshun.mongodb.net/?retryWrites=true&w=majority")
+db = client['coworking']
+collection = db['tst']
+
+data = collection.find_one()
+
+def write_data(data):
+    collection.replace_one({}, data, upsert=True)
 
 class users(BaseModel):
     userid: int
@@ -22,14 +35,15 @@ class orders(BaseModel):
     orderstop: datetime
     duration: int
 
-json_filename = "main.json"
-with open(json_filename, "r") as read_file:
-    data = json.load(read_file)
+# json_filename = "main.json"
+# with open(json_filename, "r") as read_file:
+#     data = json.load(read_file)
 
 auth = APIRouter(tags=["Authentication"],)
 user = APIRouter(tags=["Users"],)
 order = APIRouter(tags=["Orders"],)
 loca = APIRouter(tags=["Location"],)
+reserve = APIRouter(tags=["Reserve"],)
 
 
 class signin_user:
@@ -41,9 +55,9 @@ class signin_user:
     def verify_password(self, password):
         return bcrypt.verify(password, self.pass_hash)
 
-def write_data(data):
-    with open("main.json", "w") as write_file:
-        json.dump(data, write_file, indent=4)
+# def write_data(data):
+#     with open("main.json", "w") as write_file:
+#         json.dump(data, write_file, indent=4)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
@@ -116,8 +130,9 @@ async def read_all_users(user: signin_user = Depends(get_current_user)):
 
 @loca.get('/test')
 async def loc():
-	g=geocoder.ip('me')
-	return g.latlng
+    alamat = get_location()["ip"]
+    dpt = geocoder.ip(alamat)
+    return dpt.latlng
 
 @order.get('/orders')
 async def read_all_orders(user: signin_user = Depends(get_current_user)):
@@ -141,8 +156,9 @@ async def add_user(userid: int, firstname: str, lastname: str, email: str):
 
 		data['users'].append(new_user)
 
-		with open("main.json","w") as write_file:
-			json.dump(data, write_file, indent=4)
+		# with open("main.json","w") as write_file:
+		# 	json.dump(data, write_file, indent=4)
+		write_data(data)
 
 		return "User ID " + str(userid) + " is added successfully."
 	
@@ -152,40 +168,45 @@ async def add_user(userid: int, firstname: str, lastname: str, email: str):
 
 @order.post('/orders')
 async def add_order(orderid: int, userid: int, user: signin_user = Depends(get_current_user)):
-	item_found = False
-	for order_item in data['orders']:
-		if order_item['orderid'] == orderid:
-			item_found = True
-			return "Order ID "+str(orderid)+" is already exists."
-	if not item_found:
-		latitude=-7
-		longitude=108
-		g=geocoder.ip('me')
-		orderstart=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		duration=0
-		while ((str(g.lat)==str(latitude)) and (str(g.lng)==str(longitude))):
-			g=geocoder.ip('me')
-			time.sleep(1)
-			duration+=1
-		orderstop=datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-		new_order = {
-			"orderid": orderid,
-			"userid": userid,
-			"orderstart":orderstart,
-			"orderstop":orderstop,
-			"duration":duration
-		}
+    item_found = False
+    for order_item in data['orders']:
+        if order_item['orderid'] == orderid:
+            item_found = True
+            return "Order ID " + str(orderid) + " is already exists."
+    if not item_found:
+        latitude = -7
+        longitude = 108
+        alamat = get_location()["ip"]
+        dpt = geocoder.ip(alamat)
+        currentlat = dpt.lat
+        currentlong = dpt.lng
+        orderstart = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        duration = 0
+        while ((str(currentlat) == str(latitude)) and (str(currentlong) == str(longitude))):
+            time.sleep(1)
+            duration += 1
+        orderstop = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        new_order = {
+            "orderid": orderid,
+            "userid": userid,
+            "orderstart": orderstart,
+            "orderstop": orderstop,
+            "duration": duration
+        }
 
-		data['orders'].append(new_order)
+        data['orders'].append(new_order)
 
-		with open("main.json","w") as write_file:
-			json.dump(data, write_file, indent=4)
+        # with open("main.json", "w") as write_file:
+        #     json.dump(data, write_file, indent=4)
+        write_data(data)
 
-		return "User ID " + str(userid) + " is added successfully."
-	
-	raise HTTPException(
-		status_code=404, detail=f'item not found'
-	)
+        return "User ID " + str(userid) + " is added successfully."
+
+    raise HTTPException(
+        status_code=404, detail=f'item not found'
+    )
+
+
     
 @order.delete('/orders/{orderid}')
 async def delete_order(orderid: int, user: signin_user = Depends(get_current_user)):
@@ -193,8 +214,9 @@ async def delete_order(orderid: int, user: signin_user = Depends(get_current_use
 		if order_item['orderid'] == orderid:
 			data['orders'].remove(order_item)
 
-			with open("main.json","w") as write_file:
-				json.dump(data, write_file, indent=4)
+			# with open("main.json","w") as write_file:
+			# 	json.dump(data, write_file, indent=4)
+			write_data(data)
 			return "Order with ID " + str(orderid) + " deleted successfully."
 
 	return "Order ID not found."
@@ -203,8 +225,17 @@ async def delete_order(orderid: int, user: signin_user = Depends(get_current_use
 	status_code=404, detail=f'item not found'
 	)
 
+@reserve.get('/reserve')
+async def get_reservasi(user: signin_user = Depends(get_current_user)):
+    list_reservasi = []
+    for i in get_reservations():
+        list_reservasi.append(i)
+
+    return list_reservasi
+
 
 app.include_router(auth)
 app.include_router(user)
 app.include_router(order)
 app.include_router(loca)
+app.include_router(reserve)
